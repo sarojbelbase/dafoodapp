396d235bbfcfe8fb234271f106d950cb853fbf3d
Siddharth Belbase <siddharthbelbase@gmail.com>
2021-03-16 13:24:14 +0545

    ➕ introduce actions

--- store/index.js
+++ store/index.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 export const state = () => ({
     fooddata: []
 })
@@ -14,8 +16,23 @@ export const state = () => ({
 //   }
 // }
 
-// export const actions = {
-//   updateActionValue({ commit }) {
-//     commit('updateValue', payload)
-//   }
-// }
+
+export const actions = {
+    async getFoodData({ state, commit }) {
+        try {
+            const url = 'https://dva9vm8f1h.execute-api.us-east-2.amazonaws.com/production/restaurants';
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-api-key": process.env.AWS_API_KEY
+                }
+            }
+            await axios.get(url, config).then(res => Response.json()).then(data => {
+                console.log(data)
+                commit('updateFoodData', data)
+            })
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
