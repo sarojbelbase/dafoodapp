8a00b777632aaa83212b2e5b0df4096b26ecf6ce
Siddharth Belbase <siddharthbelbase@gmail.com>
2021-03-16 13:28:24 +0545

    🔒 add mutations

--- store/index.js
+++ store/index.js
@@ -10,15 +10,16 @@ export const state = () => ({
 //   }
 // }
 
-// export const mutations = {
-//   updateValue: (state, payload) => {
-//     state.value = payload
-//   }
-// }
+export const mutations = {
+    updateFoodData: (state, data) => {
+        state.fooddata = data
+    }
+}
 
 
 export const actions = {
     async getFoodData({ state, commit }) {
+        if (state.fooddata.length) return
         try {
             const url = 'https://dva9vm8f1h.execute-api.us-east-2.amazonaws.com/production/restaurants';
             const config = {
