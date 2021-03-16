1c1aa75c8377f4c755243c2a43c998fed194458a
Siddharth Belbase <siddharthbelbase@gmail.com>
2021-03-16 12:11:44 +0545

    ➕ add restaurantinfo

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <template>
   <main class="container">
     <appheader />
+    <restaurantinfo />
   </main>
 </template>
 
 <script>
 import appheader from "@/components/appheader.vue";
+import restaurantinfo from "@/components/restaurantinfo.vue";
 
 export default {
   components: {
+    restaurantinfo,
     appheader
   }
 };
 </script>
-
-<style>
-</style>
